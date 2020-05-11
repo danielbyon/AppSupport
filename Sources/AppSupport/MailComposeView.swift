32e8e6206920bafa80c1f3e205ffc3a293f736e9
Daniel Byon <daniel@danielbyon.com>
2020-05-11 03:55:32 -0700

    Add email components

--- Sources/AppSupport/MailComposeView.swift
+++ Sources/AppSupport/MailComposeView.swift
@@ -22,7 +22,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-#if os(iOS)
+#if os(iOS) || targetEnvironment(macCatalyst)
 
 import SwiftUI
 import MessageUI
